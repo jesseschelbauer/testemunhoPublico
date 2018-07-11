b1be7a1f3fb539303aec785e3dcd401b87b6f7d4
jesseschelbauer <jesseschelbauer@gmail.com>
2018-07-10 21:50:49 -0300

    Criação do formulário de cadastro das escalas
    Criação do formulário de cadastro das escalas

--- src/pages/Escala/escala.ts
+++ src/pages/Escala/escala.ts
@@ -1,36 +1,49 @@
 import { Component, Input } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Escala, Dia, DayOfWeek } from '../../app/models/escala';
+import { Escala, Dia, DayOfWeek, Periodo } from '../../app/models/escala';
 
 @Component({
   selector: 'page-escala',
   templateUrl: 'escala.html',
 })
 export class EscalaPage {
-  
+
   days: Array<Dia>;
-  @Input() public escala : Escala;
+  public periodo: Periodo;
+
+  @Input() public escala: Escala;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    if(!this.escala)
+    if (!this.escala)
       this.escala = new Escala();
 
-      this.days = new Array<Dia>();
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.sunday, descricao: "Domingo" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.monday, descricao: "Segunda-feira" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.tuesday, descricao: "Terça-feira" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.wednesday, descricao: "Quarta-feira" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.thursday, descricao: "Quinta-feira" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.friday, descricao: "Sexta-feira" });
-    this.days.push(<Dia>{ id: 0, diaDaSemana : DayOfWeek.saturday, descricao: "Sábado" });
+    this.periodo = new Periodo();
 
-    
+    this.days = new Array<Dia>();
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.sunday, descricao: "Domingo" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.monday, descricao: "Segunda-feira" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.tuesday, descricao: "Terça-feira" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.wednesday, descricao: "Quarta-feira" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.thursday, descricao: "Quinta-feira" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.friday, descricao: "Sexta-feira" });
+    this.days.push(<Dia>{ id: 0, diaDaSemana: DayOfWeek.saturday, descricao: "Sábado" });
+
+
   }
 
   ionViewDidLoad() {
   }
 
-  public addDay(dia:Dia){
-    
+  public addDay(dia: Dia) {
+
+  }
+
+  public addHorario() {
+    this.escala.periodos.push(Object.assign({}, this.periodo));
+  }
+
+  public removerPeriodo(periodo:Periodo){
+    let index = this.escala.periodos.findIndex(p => p.horaInicio == periodo.horaInicio && p.horaFim == periodo.horaFim);
+    this.escala.periodos.splice(index,1);
   }
 }
